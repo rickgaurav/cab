b4bd46433a27257f4300b34bdbb1b12c42a7f15d
Gaurav Kumar <gaurav@recruiterbox.com>
2017-09-23 14:30:33 +0530

    Add Request Model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from driver.models import Driver
+from customer.models import Customer
 
-# Create your models here.
+
+class Request(models.Model):
+
+    STATUSES = (
+        ('WAITING', 'WAITING'),
+        ('ONGOING', 'ONGOING'),
+        ('FINISHED', 'FINISHED')
+    )
+
+    driver = models.ForeignKey(Driver)
+    customer = models.ForeignKey(Customer)
+    created_at = models.DateTimeField(auto_now=True)
+    accepted_at = models.DateTimeField(null=True)
+    status = models.CharField(max_length=30, choices=STATUSES)
+
