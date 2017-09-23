d8440732fcd5315f2725bd2fac532cdd0698fd74
Gaurav Kumar <gaurav@recruiterbox.com>
2017-09-23 14:30:16 +0530

    Add driver model

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    email = models.EmailField(null=False)
+
