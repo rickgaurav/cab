268ca9e7af4a4ad408c12d99c7f0126236ab368f
Gaurav Kumar <gaurav@recruiterbox.com>
2017-09-24 01:46:42 +0530

    Namspace dshboard app urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from dashboard import views
 
+app_name = 'dashboard'
 urlpatterns = [
-    url(r'^trips/$', views.TripList.as_view()),
-    url(r'^trips/(?P<pk>[0-9]+)/$', views.TripDetail.as_view()),
+    url(r'^trips/$', views.TripList.as_view(), name='trip_list'),
+    url(r'^trips/(?P<pk>[0-9]+)/$', views.TripDetail.as_view(), name='trip_detail'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
